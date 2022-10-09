9c5f9edea4ba91484ad327227f1ee1ccc5612973
Rohit Shaw <carpediem212045@gmail.com>
2022-10-09 09:53:37 +0530

    Create My Calender 3.cpp

---
+++ My Calender 3.cpp
@@ -0,0 +1,18 @@
+class MyCalendarThree {
+public:
+    MyCalendarThree() { }
+
+    int book(int start, int end){
+        lines[start]++;
+        lines[end]--;
+        int mx = 0, cnt = 0;
+        for(auto x: lines){
+            cnt += x.second;
+            mx = max(mx, cnt);
+        }
+        return mx;
+    }
+    
+private:
+    map<int, int> lines;
+};
